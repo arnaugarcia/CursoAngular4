67a784cdf1eb1be06c2aa70c083a0d8205bb949b
Arnau García <arnau.garcia.gallego@gmail.com>
2017-04-15 17:55:44 +0200

    Crear un componente

---
+++ src/app/fruta/fruta.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by arnau on 15/4/17.
+ */
+import { Component } from '@angular/core';
+@Component({
+  selector: 'fruta',
+  templateUrl: './fruta.component.html'
+})
+export class FrutaComponent{
+ public nombre_componente = 'Componente de fruta';
+ public listado_frutas = 'Naranja, Manzan, Pera y Sandia';
+}
