1c3c78c302a0e9d057515b4e82bebdbffed21399
Arnau García <arnau.garcia.gallego@gmail.com>
2017-04-25 17:22:40 +0200

    Propiedades y tipos de datos

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -2,11 +2,17 @@
  * Created by arnau on 15/4/17.
  */
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'fruta',
   templateUrl: './fruta.component.html'
 })
 export class FrutaComponent{
  public nombre_componente = 'Componente de fruta';
- public listado_frutas = 'Naranja, Manzan, Pera y Sandia';
+ public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
+
+ public nombre:string = 'Arnau García';
+ public edad:number = 20;
+ public mayordDeEdad:boolean = true;
+ public trabajos:Array<any> = ['Programador', 44, 'Project Manager'];
 }
