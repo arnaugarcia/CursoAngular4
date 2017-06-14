0541435ed66b2bbed4877e570b3b3a3f1994217e
Arnau García <arnau.garcia.gallego@gmail.com>
2017-06-14 20:30:18 +0200

    Menú de navegación

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,12 +7,16 @@ import { Routes, RouterModule } from '@angular/router';
 // Importar componentes propios
 import { EmpleadosComponent } from './empleado/empleado.component';
 import { FrutaComponent } from './fruta/fruta.component';
+import {ContactoComponent} from './contacto/contacto.component';
+import {HomeComponent} from './home/home.component';
 
 const appRoutes: Routes = [
-  {path: '', component: EmpleadosComponent},
+  {path: '', component: HomeComponent},
   {path: 'empleado', component: EmpleadosComponent},
   {path: 'fruta', component: FrutaComponent},
-  {path: '**', component: EmpleadosComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: '**', component: HomeComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by arnau on 14/6/17.
+ */
+import { Component } from '@angular/core';
+@Component({
+  selector: 'contacto',
+  templateUrl: 'contacto.component.html'
+})
+export class ContactoComponent {
+  public titulo = 'Página de contacto';
+}
