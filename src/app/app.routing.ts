ba2442947d8b57a30e4aaf6fcad7ca5185b2bbb3
Arnau García <arnau.garcia.gallego@gmail.com>
2017-06-15 16:30:44 +0200

    Rutas y parámetros por la URL

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -16,6 +16,7 @@ const appRoutes: Routes = [
   {path: 'fruta', component: FrutaComponent},
   {path: 'home', component: HomeComponent},
   {path: 'contacto', component: ContactoComponent},
+  {path: 'contacto/:page', component: ContactoComponent},
   {path: '**', component: HomeComponent}
 ];
 
--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -2,10 +2,24 @@
  * Created by arnau on 14/6/17.
  */
 import { Component } from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 @Component({
   selector: 'contacto',
   templateUrl: 'contacto.component.html'
 })
 export class ContactoComponent {
   public titulo = 'Página de contacto';
+  public parametro;
+
+  constructor (
+    private _route: ActivatedRoute,
+    private _router: Router,
+  ){}
+
+  ngOnInit() {
+    this._route.params.forEach((params: Params) => {
+      console.log(params);
+      this.parametro = params['page'];
+    });
+  }
 }
