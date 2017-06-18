6ac6c9b4afebc43e3d9b7c28d4f76390cf6602a6
Arnau García <arnau.garcia.gallego@gmail.com>
2017-06-18 20:21:06 +0200

    Ejercicio con los servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,26 @@ import {RopaService} from '../services/ropa.service';
 })
 export class HomeComponent {
   public titulo = 'Página principal';
+  public listado_ropa: Array<string>;
+  public prenda_a_guardar: string;
 
   constructor (private _ropaService: RopaService) {}
 
   ngOnInit() {
+    this.listado_ropa = this._ropaService.getRopa();
+    console.log(this.listado_ropa);
   console.log(this._ropaService.prueba('Camiseta Nike'));
   }
 
+  guardarPrenda() {
+    this._ropaService.addRopa(this.prenda_a_guardar);
+    this.prenda_a_guardar = null;
+  }
+
+  eliminarPrenda(index: number) {
+    alert(index);
+    this._ropaService.eliminarPrenda(index);
+  }
 
 
 }
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -8,8 +8,24 @@ export class RopaService {
 
   public nombre_prenda = 'Pantalones vaqueros';
 
+  public colecion_ropa = ['Pantalones Blancos', 'Camiseta Roja'];
+
   prueba(nombre_prenda) {
     return nombre_prenda;
+  }
+
+  addRopa(nombre_prenda: string): Array<String> {
+    this.colecion_ropa.push(nombre_prenda);
+    return this.getRopa();
+  }
+
+  getRopa(): Array<string> {
+    return this.colecion_ropa;
+  }
+
+  eliminarPrenda(index: number): Array<string> {
+    this.colecion_ropa.splice(index, 1);
+    return this.getRopa();
   }
 
 }
