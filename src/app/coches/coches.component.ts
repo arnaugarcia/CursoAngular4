2b008dc65e9486d4ef18fb323004e3eeafc24b62
Arnau García <arnau.garcia.gallego@gmail.com>
2017-06-25 14:28:36 +0200

    Ejercicio con formularios PT1

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -9,12 +9,15 @@ import {Coche} from './coche';
 })
 export class CochesComponent {
   public coche: Coche;
+  public coches: Array<Coche>;
 
   constructor() {
     this.coche = new Coche('', 0, '');
+    this.coches = [new Coche('Seat Panda', 120, 'Blanco'), new Coche('Renault Clio', 110, 'Azul')];
   }
 
   onSubmit() {
-    console.log(this.coche);
+    this.coches.push(this.coche);
+    this.coche = new Coche('', 0, '');
   }
 }
