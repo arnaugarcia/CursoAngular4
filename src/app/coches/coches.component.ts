2c538ad919a243ee7e8194d2d9fd87a0bd601e53
Arnau García <arnau.garcia.gallego@gmail.com>
2017-06-28 19:58:41 +0200

    Crear servicio para peticiones HTTP

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -3,17 +3,25 @@
  */
 import {Component} from '@angular/core';
 import {Coche} from './coche';
+import {PeticionesService} from '../services/peticiones.service';
 @Component({
   selector: 'coches',
-  templateUrl: './coches.component.html'
+  templateUrl: './coches.component.html',
+  providers: [PeticionesService]
 })
 export class CochesComponent {
   public coche: Coche;
   public coches: Array<Coche>;
 
-  constructor() {
+  constructor(
+    private _peticionesService: PeticionesService
+  ) {
     this.coche = new Coche('', 0, '');
     this.coches = [new Coche('Seat Panda', 120, 'Blanco'), new Coche('Renault Clio', 110, 'Azul')];
+  }
+
+  ngOnInit() {
+    console.log(this._peticionesService.getPrueba());
   }
 
   onSubmit() {
---
+++ src/app/services/peticiones.service.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by arnau on 26/6/17.
+ */
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
+@Injectable()
+export class PeticionesService {
+
+  getPrueba() {
+    return 'Hola mundo desde el servicio';
+  }
+
+}
